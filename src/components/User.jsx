26ff6c81743a473e72b54f15cd768ae70d1379b6
NatClamp <nat.clamp@gmail.com>
2019-01-12 14:37:52 +0000

    creating and styling basic user page

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './User.css';
 
-const Users = () => {
+const Users = props => {
+  const { username, avatar_url, name } = props.user;
   return (
-    <main className='blackborder'>
-      <h1>User Component</h1>
+    <main className='content-well'>
+      <article className='userCard'>
+        <section className='userCard__avatarContainer'>
+          <img src={avatar_url} alt='user_avatar' className='user_avatar' />
+        </section>
+        <section className='userCard__textContainer'>
+          <h1 className='userName'>{name}</h1>
+          <h2 className='userHandle'>@{username}</h2>
+        </section>
+      </article>
     </main>
   );
 };
